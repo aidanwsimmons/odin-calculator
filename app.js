4414d75e1c438168632436053226e0fe914ebea6
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-04-28 17:33:46 -0700

    added btn vars to app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+let num1 = 0
+let num2 
+let operator
+let displayVal
+
+const btn1 = document.querySelector('.one')
+const btn2 = document.querySelector('.two')
+const btn3 = document.querySelector('.three')
+const btn4 = document.querySelector('.four')
+const btn5 = document.querySelector('.five')
+const btn6 = document.querySelector('.six')
+const btn7 = document.querySelector('.seven')
+const btn8 = document.querySelector('.eight')
+const btn9 = document.querySelector('.nine')
+const btn0 = document.querySelector('.zero')
+const btnAdd = document.querySelector('.add')
+const btnSubtract = document.querySelector('.subtract')
+const btnMultiply = document.querySelector('.multiply')
+const btnDivide = document.querySelector('.divide')
+const btnEquals = document.querySelector('.equals')
+const btnPoint = document.querySelector('.point')
+const btnClear = document.querySelector('.clear')
+const btnDelete = document.querySelector('.delete')
+
+
+// function add {
+
+// }
+
+// function subtract {
+
+// }
+
+// function multiply {
+
+// }
+
+// function divide {
+
+// }
+
+// function operate(operator, num1, num2) {
+
+// }
